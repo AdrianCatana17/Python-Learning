b7dc6d86b13c0b7b3a91b3949610c765615deaf7
Adrian Catana <mariancatana1412@gmail.com>
2024-03-26 20:46:30 +0200

    Update RockPaperScissors.py
    Commenting description

--- RockPaperScissors/RockPaperScissors.py
+++ RockPaperScissors/RockPaperScissors.py
@@ -1,19 +1,19 @@
 import random
 
 def play():
-    user = input("What's your choice?: 'r' for rock, 'p' for paper, 's' for scissors.\n")
-    computer = random.choice(['r','p','s'])
+    user = input("What's your choice?: 'r' for rock, 'p' for paper, 's' for scissors.\n") # Ask for user input
+    computer = random.choice(['r','p','s']) # computer makes a random choice from the list
     
-    if user == computer:
+    if user == computer:  # If user made the same choice as the computer, then it's a tie
         return("It's a tie!")
       
     # r >s, s > p, p > r
     if is_win(user, computer):
         return "You won!"
         
-    return 'You lost!'
+    return 'You lost!' 
 
-def is_win(player, opponent):
+def is_win(player, opponent): # define a function for user winning choices
     if(player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
     
